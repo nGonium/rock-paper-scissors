b3e81de70bea51da96620fe1a584fa04cc164ab4
Didi nGon- <ngon-@hotmail.com>
2022-04-09 19:47:26 +0100

    Add multi-round console game

--- main.js
+++ main.js
@@ -8,25 +8,35 @@ const WINCONDITIONS = {
 game();
 
 function game(){
-    let playerSelection = prompt('Select: Rock, Paper or Scissors?')
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
-    while(!(playerSelection in WINCONDITIONS)){
-        playerSelection = prompt('Invalid selection, select again: Rock, Paper or Scissors?')
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
-    }
-    let computerSelection = computerPlay()
+    const ROUNDS = 5;
+    let score = 0;
+    for(i = 0; i < ROUNDS; i++){
+        let playerSelection = prompt('Select: Rock, Paper or Scissors?')
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        while(!(playerSelection in WINCONDITIONS)){
+            playerSelection = prompt('Invalid selection, select again: Rock, Paper or Scissors?');
+            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        }
+        let computerSelection = computerPlay();
 
-    let round = playRound(playerSelection, computerSelection)
-    console.log(playerSelection, computerSelection, round);
+        let round = playRound(playerSelection, computerSelection);
+        score += round;
+        console.log(
+            'Player: ' + playerSelection, 
+            '\nComputer: ' + computerSelection, 
+            '\nScore: ' + (round > 0 ? '+' + round : round),
+            '\nTotal score: ' + score);
+    }
+ 
 }
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection) return 'DRAW';
-    if(WINCONDITIONS[playerSelection] === computerSelection) return 'PLAYERWIN';
-    return 'COMPUTERWIN';
+    if(playerSelection === computerSelection) return 0;
+    if(WINCONDITIONS[playerSelection] === computerSelection) return 1;
+    return -1;
 }
 
 function computerPlay(){
-    const choices = ['Rock', 'Paper', 'Scissors']
+    const choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * 3)];
 }
