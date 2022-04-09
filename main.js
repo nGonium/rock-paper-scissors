4a879e806c4675d5959c7d3a9e1da9e41e55876f
Didi nGon- <ngon-@hotmail.com>
2022-04-09 19:30:46 +0100

    Add one-round console game

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-computerPlay()
+//Player : computer selection pairs that result in player victory
+const WINCONDITIONS = {
+    'Rock': 'Scissors',
+    'Paper': 'Rock',
+    'Scissors': 'Rock'
+}
+
+game();
+
+function game(){
+    let playerSelection = prompt('Select: Rock, Paper or Scissors?')
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    while(!(playerSelection in WINCONDITIONS)){
+        playerSelection = prompt('Invalid selection, select again: Rock, Paper or Scissors?')
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    }
+    let computerSelection = computerPlay()
+
+    let round = playRound(playerSelection, computerSelection)
+    console.log(playerSelection, computerSelection, round);
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection) return 'DRAW';
+    if(WINCONDITIONS[playerSelection] === computerSelection) return 'PLAYERWIN';
+    return 'COMPUTERWIN';
+}
 
 function computerPlay(){
-
+    const choices = ['Rock', 'Paper', 'Scissors']
+    return choices[Math.floor(Math.random() * 3)];
 }
